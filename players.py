256d4f4f28d6a7c8705fad05f77fb2d31793f0c9
Jose David <josedavidsanchez29@gmail.com>
2021-03-22 12:00:48 -0400

    Doing the checking card

--- players.py
+++ players.py
@@ -4,4 +4,10 @@ class Players:
         self.score =0
         self.hand = []
         self.off_hand = []
-    
+    def play(self,table):
+      card_playing = input("what card are you gonna play?")
+      def check_cards(Input,list_of_cards):
+        for card in list_of_cards:
+            if Input == card.name:
+                return card
+        print ("Card is not found")
