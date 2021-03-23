568dd82d10e554ec9f3ca8e76ec9e19dd736b834
Jose David <josedavidsanchez29@gmail.com>
2021-03-23 13:52:19 -0400

    add a form to play with positions

--- player.py
+++ player.py
@@ -6,13 +6,21 @@ class Player:
         self.off_hand = []
     def play(self,table):
         card_playing = input("what card are you gonna play?")
-      
+
         def check_cards(Input,list_of_cards):
-            for card in list_of_cards:
-                if Input == card.name:
+            try:
+                H = int(Input)
+                if H < len(list_of_cards):
                     print("Success")
-                    return card
-            print ("Card is not found")
-            self.play(table)
-        
+                    return list_of_cards[H]
+                else:
+                    print("Input is out of range")
+                    self.play(table)
+            except:
+                for card in list_of_cards:
+                    if Input == card.name:
+                        print("Success")
+                        return card
+                print ("Card is not found")
+                self.play(table)
         card_playing = check_cards(card_playing,self.hand)
