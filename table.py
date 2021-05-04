7ef3356def47f2513b5e728608bb30e9bd69cfa2
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2021-05-04 19:20:53 -0400

    doing the score count

--- table.py
+++ table.py
@@ -16,3 +16,29 @@ class Table:
             for player in self.list_of_players:
                 player.hand.append(self.deck.pop())
             i += 1
+    def score(self,deck,plist,off_hand):
+        value_of_points ={
+            "A" = 1
+            "Clubs" = 1
+            "10_Diamonds" = 2
+            "2_Clubs" = 1
+        }
+        if deck ==[]:
+            loff_hand = []
+            boff_hand = 0
+            for player in plist:
+                l=0
+                loff_hand.append(player.len(off_hand))
+                club_cards = 0
+                number_of_cards = len(off_hand)
+                if loff_hand[l] > boff_hand:
+                    boff_hand = loff_hand[l]
+                    l =+1
+                    if loff_hand.count(boff_hand) = 1:
+                        off_hand_points = loff_hand.index(boff_hand)
+                        player[off_hand_points].score =+ 2
+                for i in off_hand:
+                    if i.symbol == "Clubs":
+                        club_cards =+ 1
+                    if i.value =="A" or i.name == "10_Diamonds" or i.name == "2_Clubs":
+                        player.score =+ 1
